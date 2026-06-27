9348be238fb876157e602cfc777bdbba341043b9
shariq6dispatcher-eng <shariq6dispatcher@gmail.com>
2026-06-27 12:21:32 -0500

    Update mongodb.ts

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI;
 
 if (!uri) {
   throw new Error("Missing MONGODB_URI");
@@ -10,20 +10,17 @@ let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 declare global {
-  var _mongoClientPromise: Promise<MongoClient>;
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri);
-
     global._mongoClientPromise = client.connect();
   }
-
   clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri);
-
   clientPromise = client.connect();
 }
 
