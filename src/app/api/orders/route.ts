9ff8ec02ec895462598cebc8fbcb5357ec6959b6
shariq6dispatcher-eng <shariq6dispatcher@gmail.com>
2026-06-30 14:21:34 -0500

    Update route.ts

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,33 +1,33 @@
-import clientPromise from "@/lib/mongodb";
-import { NextResponse } from "next/server";
+// import clientPromise from "@/lib/mongodb";
+// import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
 
-    const client = await clientPromise;
-    const db = client.db("goventure");
+//     const client = await clientPromise;
+//     const db = client.db("goventure");
 
-    await db.collection("orders").insertOne({
-      customerName: body.customerName,
-      email: body.email,
-      phone: body.phone,
-      product: body.product,
-      quantity: body.quantity,
-      notes: body.notes,
-      createdAt: new Date(),
-      status: "Pending",
-    });
+//     await db.collection("orders").insertOne({
+//       customerName: body.customerName,
+//       email: body.email,
+//       phone: body.phone,
+//       product: body.product,
+//       quantity: body.quantity,
+//       notes: body.notes,
+//       createdAt: new Date(),
+//       status: "Pending",
+//     });
 
-    return NextResponse.json({
-      success: true,
-    });
-  } catch (error) {
-    console.error(error);
+//     return NextResponse.json({
+//       success: true,
+//     });
+//   } catch (error) {
+//     console.error(error);
 
-    return NextResponse.json(
-      { success: false },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(
+//       { success: false },
+//       { status: 500 }
+//     );
+//   }
+// }
