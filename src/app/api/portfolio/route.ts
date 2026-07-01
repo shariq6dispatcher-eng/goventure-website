57c1a7c016fe9bb178c8edd02f8a6e2db91b5259
shariq6dispatcher-eng <shariq6dispatcher@gmail.com>
2026-07-01 13:23:14 -0500

    Update route.ts

--- src/app/api/portfolio/route.ts
+++ src/app/api/portfolio/route.ts
@@ -1,56 +1,14 @@
-import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  const body = await req.json();
-
-  const client = await clientPromise;
-  const db = client.db("goventure");
-
-  await db.collection("portfolio").insertOne({
-    title: body.title,
-    image: body.image,
-    category: body.category,
-    createdAt: new Date(),
-  });
-
+export async function GET() {
   return NextResponse.json({
     success: true,
+    message: "Portfolio API is alive",
   });
 }
 
-export async function GET(req: Request) {
-  try {
-    const client = await clientPromise;
-    const db = client.db("goventure");
-
-    const { searchParams } = new URL(req.url);
-
-    const category = searchParams.get("category");
-
-    let query = {};
-
-    if (category) {
-      query = {
-        category: {
-          $regex: new RegExp(category, "i"),
-        },
-      };
-    }
-
-    const portfolio = await db
-      .collection("portfolio")
-      .find(query)
-      .sort({ createdAt: -1 })
-      .toArray();
-
-    return NextResponse.json(portfolio);
-  } catch (error) {
-    console.error(error);
-
-    return NextResponse.json(
-      { error: "Database Error" },
-      { status: 500 }
-    );
-  }
+export async function POST() {
+  return NextResponse.json({
+    success: true,
+  });
 }
