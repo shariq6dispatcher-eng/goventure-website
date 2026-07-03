5596afe47c0e849035add7d0b2ed5e9ed6d68f55
shariq6dispatcher-eng <shariq6dispatcher@gmail.com>
2026-07-03 12:07:49 -0500

    Update page.tsx

--- src/app/Portfolio/page.tsx
+++ src/app/Portfolio/page.tsx
@@ -1,20 +1,20 @@
 import PageHero from "@/components/ui/common/PageHero";
 import CategoryPreview from "@/components/ui/gallery/CategoryPreview";
 import { mongo } from "@/lib/mongodb";
-
+ 
 async function getPortfolio() {
   return await mongo.find("portfolio", {}, { createdAt: -1 });
 }
 export default async function PortfolioPage() {
   const projects = await getPortfolio();
-
+ 
   const categoriesMap = new Map<string, any>();
-
-  for (const p of projects) {
+ 
+  for (const p of projects as any[]) {
   if (!p.image) continue;
-
-  const key = p.category;
-
+ 
+  const key = p.category as string;
+ 
   if (
     !categoriesMap.has(key) ||
     new Date(p.createdAt) >
@@ -23,12 +23,12 @@ export default async function PortfolioPage() {
     categoriesMap.set(key, p);
   }
 }
-
+ 
   const categories = Array.from(categoriesMap.values()).map((p: any) => ({
     category: p.category,
     image: p.image,
   }));
-
+ 
   return (
     <main className="bg-black text-white">
       <PageHero
