93499ee22a2ea9e9d65ae2fbddb1ea635e0aed0b
shariq6dispatcher-eng <shariq6dispatcher@gmail.com>
2026-07-16 14:02:05 -0500

    Update RsmSidebar.tsx

--- src/components/admin/rsm/RsmSidebar.tsx
+++ src/components/admin/rsm/RsmSidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import {
